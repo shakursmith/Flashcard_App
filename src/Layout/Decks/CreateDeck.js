3b4afd24898456c00a017053cf1787c97b2f9c26
Shakur Smith <smith.shakur@ymail.com>
2021-02-15 16:22:49 -0600

    first commit

---
+++ src/Layout/Decks/CreateDeck.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link, useHistory } from 'react-router-dom';
+import { createDeck } from '../../utils/api/index.js'
+
+function CreateDeck({updateDecks}) {
+    const [newDeck, setNewDeck] = useState({name: "", description: ""});
+    const history = useHistory();
+
+    const formChange = ({ target }) => {
+        setNewDeck({...newDeck, [target.name]: target.value});
+    }
+    
+    const formSubmit = async (event) => {
+        event.preventDefault();
+        const response = await createDeck(newDeck);
+        history.push(`/decks/${response.id}`);
+        updateDecks(1);
+    }
+
+    return (
+        <div className="col-9 mx-auto">
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item"><Link to={"/"}><i className="fa fa-home" aria-hidden="true"></i> Home</Link></li>
+                    <li className="breadcrumb-item">Create Deck</li>
+                </ol>
+            </nav>
+            <form onSubmit={formSubmit}>
+                <div class="form-group">
+                    <label>Name</label>
+                    <input 
+                    type="text" 
+                    name="name"
+                    value={newDeck.name}
+                    onChange={formChange}
+                    id="name" 
+                    class="form-control" 
+                    placeholder="Deck Name" 
+                    />
+                </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea
+                    name="description" 
+                    value={newDeck.description}
+                    onChange={formChange}
+                    class="form-control" 
+                    id="description" 
+                    placeholder="Brief description of the deck."
+                    rows={4}
+                    />
+                </div>
+                <Link to="/" name="cancel" class="btn btn-secondary mr-3">Cancel</Link>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateDeck;
+                

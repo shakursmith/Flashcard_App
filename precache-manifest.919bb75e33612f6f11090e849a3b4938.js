43234ce26c224d92a85e2117c424ae1a85a05be3
Shakur Smith <smith.shakur@ymail.com>
2021-02-15 16:31:56 -0600

    Updates

---
+++ precache-manifest.919bb75e33612f6f11090e849a3b4938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ae6d733bfc72d63600b7da6ce4d287",
+    "url": "/Flashcard_App/index.html"
+  },
+  {
+    "revision": "7ab835ff01706a387c9a",
+    "url": "/Flashcard_App/static/js/2.f1f79d8a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Flashcard_App/static/js/2.f1f79d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe45b0bf7f961d9b160",
+    "url": "/Flashcard_App/static/js/main.186f2cce.chunk.js"
+  },
+  {
+    "revision": "ba23b2573a882f9fbcf8",
+    "url": "/Flashcard_App/static/js/runtime-main.9c2cd4cf.js"
+  }
+]);
